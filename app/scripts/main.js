787750efb8af4b183bcb0138d0fcda50498e30e5
Chelsea Franz <chelseafranz3@gmail.com>
2015-10-15 12:51:56 -0400

    styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,3 +4,7 @@ $(document).foundation('reflow');
 $('#folderBtn').on('click', function () {
     $(this).siblings('.addBtn').toggleClass('mui-hide');
 });
+
+$('.sign').on('click', function(){
+  $('.introCard').toggleClass('hide');
+})
