223df239b35099a7149d3cb5053516dffc4f3306
Chelsea Franz <chelseafranz3@gmail.com>
2015-10-16 13:22:25 -0400

    navigation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,3 +8,18 @@ $('#folderBtn').on('click', function () {
 $('.sign').on('click', function(){
   $('.introCard').toggleClass('hide');
 })
+
+$('a').on('click', function () {
+
+    $(this).siblings('.addBtn').toggleClass('mui-hide');
+});
+
+$('.folder').on('click', function () {
+  $('#folderView').removeClass('hide');
+  $('#docView').addClass('hide');
+});
+
+$('.document').on('click', function () {
+  $('#docView').removeClass('hide');
+  $('#folderView').addClass('hide');
+});
